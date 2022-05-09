ef0ddadf1d1610a2ae7ba481a28fbcb497faba44
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2022-05-09 18:56:04 +0000

    feat: basic site detailing routes

--- site/online.js
+++ site/online.js
@@ -1,7 +1,3 @@
-// <!-- fetch('http://example.com/movies.json') -->
-// <!-- .then(response => response.json()) -->
-// <!-- .then(data => console.log(data)); -->
-
 const zeroPad = (unit) => String(unit).padStart(2, '0');
 
 const secondsToText = (s) => {
@@ -9,31 +5,26 @@ const secondsToText = (s) => {
 	const minute = zeroPad(Math.floor(s / 60 % 60));
 	const hour = zeroPad(Math.floor(s / 60 / 60 % 24));
 	const day = Math.floor(s / 60 / 60 / 24);
-	// return short ?
-		// `${day}d, ${hour}h, ${minute}m, ${second}s`;
-		return `${day} days, ${hour} hours, ${minute} minutes, ${second} seconds`;
+	return `${day} days, ${hour} hours, ${minute} minutes, ${second} seconds`;
 };
 
 
-async function check_status() {
-	console.log("from script");
+const  check_status = async () => {
+	const request = await fetch("https://api.adsbdb.com/v0/online");
+	const response = await request.json();
+	const uptime = document.querySelector("#uptime");
+	const api_version = document.querySelector("#api_version");
 
-	let request = await fetch("https://api.adsbdb.com/v0/online");
-	let response = await request.json();
-	// response.uptime *&& response.api_verion
-	console.log(response);
-	let uptime = document.querySelector("#uptime");
+	if (response?.response?.uptime && response?.response?.api_version) {
+		api_version.innerHTML = response.response.api_version
+		uptime.innerHTML = secondsToText(response.response.uptime)
+		document.querySelector('#stats').style.display = 'block';
 
-	// let uptime_formatted =
-	uptime.innerHTML =  secondsToText(response.response.uptime)
-
-
-	let api_version = document.querySelector("#api_version");
-	api_version.innerHTML = response.response.api_version
-
+	}
 }
 
-
-
-
 check_status()
+
+setInterval(() => {
+	check_status()
+}, 60000);
