1927d0d9324577304e400d1c6c6ff1eeb3ba8467
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2022-05-09 15:40:37 +0000

    fix: docker db restore

--- docker/init/postgres_init.sh
+++ docker/init/postgres_init.sh
@@ -3,31 +3,34 @@ set -e
 
 create_adsbdb_user() {
 	echo "create_adsbdb_user"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" -p "$DOCKER_PG_PORT" <<-EOSQL
-CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASSWORD';
+	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_USER}"  <<-EOSQL
+CREATE ROLE ${DB_USER} WITH LOGIN PASSWORD '${DB_PASSWORD}';
 EOSQL
 }
 
 create_adsbdb_database() {
 	echo "create_adsbdb_database"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" -p "$DOCKER_PG_PORT" <<-EOSQL
-CREATE DATABASE $DB_NAME;
+	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_USER}"  <<-EOSQL
+CREATE DATABASE ${DB_NAME};
 EOSQL
 }
 
+# Create db from .sql file, requires other data (*.csv etc) to read to build
 bootstrap_from_sql_file() {
-	psql -U "$POSTGRES_USER" -d postgres -f /init/init_db.sql
+	psql -U "${POSTGRES_USER}" -d postgres -f /init/init_db.sql
 }
 
+
+# restore a db from a pg_dump file
 restore_pg_dump() {
 	echo "restore_pg_dump"
-	pg_restore -U "$POSTGRES_USER" -O --exit-on-error --single-transaction -d "$DB_NAME" -p "$DOCKER_PG_PORT" -v /init/pg_dump.tar
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB_NAME" -p "$DOCKER_PG_PORT" <<-EOSQL
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO $DB_NAME;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_NAME;
+	pg_restore -U "${POSTGRES_USER}" -O --exit-on-error --single-transaction -d "${DB_NAME}" -v /init/pg_dump.tar
+	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" <<-EOSQL
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO ${DB_NAME};
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${DB_NAME};
 EOSQL
 }
-# if pg_dump then resotre, elsee from scratch
+# if pg_dump then restore_pg_dump, else from bootstrap_from_sql_file
 main () {
 
 	# If creating from scratch, with .sql file & .csv files
