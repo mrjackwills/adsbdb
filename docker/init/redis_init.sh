a4c30f3ba96cb3d5efcdc57abf7af743b723f743
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2022-07-21 15:14:30 +0000

    feat: redis conf create using .env
    Redis init script use env values to update the redis.conf

--- docker/init/redis_init.sh
+++ docker/init/redis_init.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
 
-sed -i "s|requirepass replace_me|requirepass ${DOCKER_REDIS_PASSWORD}|" /init/redis.conf
-sed -i "s|bind redis |bind ${DOCKER_REDIS_HOST}|" /init/redis.conf
+sed -i "s|# requirepass foobared|requirepass ${DOCKER_REDIS_PASSWORD}|" /init/redis.conf
+sed -i "s=bind 127.0.0.1 -::1=bind ${DOCKER_REDIS_HOST}=" /init/redis.conf
+sed -i "s=port 6379=port ${DOCKER_REDIS_PORT}=" /init/redis.conf
+sed -i "s=/var/run/redis_6379.pid=/var/run/redis_${DOCKER_REDIS_PORT}.pid=" /init/redis.conf
+sed -i "s=logfile \"\"=logfile /redis_logs/redis-server.log=" /init/redis.conf
+sed -i "s=# save 3600 1 300 100 60 10000=save 60 1=" /init/redis.conf
+sed -i "s=dir ./=dir /redis_data=" /init/redis.conf
+sed -i "s=repl-diskless-sync yes=repl-diskless-sync no=" /init/redis.conf
 
 exec "$@"
