c709446db67d31ce4235cb226ff920a532caa329
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2022-07-21 15:18:34 +0000

    feat: log to file and stdout

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use api::AppError;
 use parse_env::AppEnv;
 use tokio::sync::Mutex;
 use tracing::Level;
+use tracing_subscriber::fmt::writer::MakeWriterExt;
 
 fn setup_tracing(app_envs: &AppEnv) {
     let level = if app_envs.trace {
@@ -20,7 +21,16 @@ fn setup_tracing(app_envs: &AppEnv) {
     } else {
         Level::INFO
     };
-    tracing_subscriber::fmt().with_max_level(level).init();
+
+	let logfile = tracing_appender::rolling::never(&app_envs.location_logs, "api.log");
+    let stdout = std::io::stdout.with_max_level(level);
+
+    tracing_subscriber::fmt()
+        .with_max_level(level)
+        .with_file(true)
+        .with_line_number(true)
+        .with_writer(logfile.and(stdout))
+        .init();
 }
 
 #[tokio::main]
@@ -29,6 +39,6 @@ async fn main() -> Result<(), AppError> {
     setup_tracing(&app_env);
     let postgres = db_postgres::db_pool(&app_env).await?;
     let redis = db_redis::get_connection(&app_env).await?;
-    api::serve(app_env, postgres, Arc::new(Mutex::new(redis))).await;
+    api::serve(app_env, postgres, Arc::new(Mutex::new(redis))).await?;
     Ok(())
 }
