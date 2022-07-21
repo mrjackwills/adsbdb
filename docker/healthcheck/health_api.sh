1fcde366ea05fa22b573e810561d8be58713693d
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2022-07-21 13:58:01 -0400

    fix: api healthcheck url typo

--- docker/healthcheck/health_api.sh
+++ docker/healthcheck/health_api.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 API_PORT=$(grep "API_PORT" /app_env/.api.env | cut -c 10-13)
-URL="adsbdb_api:${API_PORT}/v1/incognito/online"
+URL="adsbdb_api:${API_PORT}/v0/online"
 wget -nv -t1 --spider "${URL}" || exit 1
