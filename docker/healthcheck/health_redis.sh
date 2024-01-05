815cde77e5bbc56421e3cf3bb7a63a9c0f8c6882
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-01-05 16:12:53 +0000

    chore: file linting

--- docker/healthcheck/health_redis.sh
+++ docker/healthcheck/health_redis.sh
@@ -2,8 +2,7 @@
 set -e
 
 PONG=$(redis-cli -h "${DOCKER_REDIS_HOST}" -p "${DOCKER_REDIS_PORT}" -a "${DOCKER_REDIS_PASSWORD}" --no-auth-warning ping)
-if expr "$PONG" : 'PONG' > /dev/null;
-then
+if expr "$PONG" : 'PONG' >/dev/null; then
 	exit 0
 else
 	exit 1
--- docker/init/postgres_init.sh
+++ docker/init/postgres_init.sh
@@ -3,16 +3,16 @@ set -e
 
 create_adsbdb_user() {
 	echo "create_adsbdb_user"
-	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_USER}"  <<-EOSQL
-CREATE ROLE ${DB_USER} WITH LOGIN PASSWORD '${DB_PASSWORD}';
-EOSQL
+	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_USER}" <<-EOSQL
+		CREATE ROLE ${DB_USER} WITH LOGIN PASSWORD '${DB_PASSWORD}';
+	EOSQL
 }
 
 create_adsbdb_database() {
 	echo "create_adsbdb_database"
-	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_USER}"  <<-EOSQL
-CREATE DATABASE ${DB_NAME};
-EOSQL
+	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_USER}" <<-EOSQL
+		CREATE DATABASE ${DB_NAME};
+	EOSQL
 }
 
 # Create db from .sql file, requires other data (*.csv etc) to read to build
@@ -25,9 +25,9 @@ restore_pg_dump() {
 	echo "restore_pg_dump"
 	pg_restore -U "${POSTGRES_USER}" -O --exit-on-error --single-transaction -d "${DB_NAME}" -v /init/pg_dump.tar
 	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" <<-EOSQL
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO ${DB_NAME};
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${DB_NAME};
-EOSQL
+		GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO ${DB_NAME};
+		GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${DB_NAME};
+	EOSQL
 }
 
 from_pg_dump() {
@@ -41,12 +41,11 @@ from_scratch() {
 	bootstrap_from_sql_file
 }
 
-main () {
-	if [ -f "/init/pg_dump.tar" ];
-	then
-		from_pg_dump;
+main() {
+	if [ -f "/init/pg_dump.tar" ]; then
+		from_pg_dump
 	else
-		from_scratch;
+		from_scratch
 	fi
 }
 
