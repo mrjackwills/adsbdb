e51cfceb3d11ba7d5f1e00865ddde92109c649d3
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-02-07 15:47:59 +0000

    refactor: sql files linted

--- src/db_postgres/migrations.sql
+++ src/db_postgres/migrations.sql
@@ -1,3 +1,14 @@
-ALTER TABLE airport_elevation ALTER COLUMN elevation TYPE INT;
-ALTER TABLE airport_longitude ALTER COLUMN longitude TYPE DOUBLE PRECISION USING longitude::double precision;
-ALTER TABLE airport_latitude ALTER COLUMN latitude TYPE DOUBLE PRECISION USING latitude::double precision;
+ALTER TABLE
+	airport_elevation
+ALTER COLUMN
+	elevation TYPE INT;
+
+ALTER TABLE
+	airport_longitude
+ALTER COLUMN
+	longitude TYPE DOUBLE PRECISION USING longitude :: double precision;
+
+ALTER TABLE
+	airport_latitude
+ALTER COLUMN
+	latitude TYPE DOUBLE PRECISION USING latitude :: double precision;
