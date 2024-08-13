70af303ab0eebb598fb7b361bd49fb241d15cc83
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-08-13 19:28:42 +0000

    fix: health_api.sh updated
    Use a more robust api health check, will check for status code 200 and an uptime key in the body

--- docker/healthcheck/health_api.sh
+++ docker/healthcheck/health_api.sh
@@ -1,5 +1,34 @@
 #!/bin/sh
 
-API_PORT=$(grep "API_PORT" /app_env/.api.env | cut -c 10-13)
-URL="adsbdb_api:${API_PORT}/v0/online"
-wget -nv -t1 --spider "${URL}" || exit 1
+main() {
+	API_PORT=$(grep "API_PORT" /app_env/.api.env | cut -c 10-13)
+	URL="adsbdb_api:${API_PORT}/v0/online"
+
+	# Make the request using wget and process the response
+	response=$(wget -S -O - --timeout=1 "$URL" 2>&1)
+
+	# Extract the status code
+	status_code=$(echo "$response" | grep -oP 'HTTP/[0-9\.]+\s+\K[0-9]+')
+
+	# Extract the uptime value from the JSON response
+	uptime=$(echo "$response" | grep -oP '\{.*\}' | grep -oP '"uptime":\K[0-9]+')
+
+	# Check if the status code is 200 and uptime is a valid number
+	if [ "$status_code" = "200" ]; then
+		case "$uptime" in
+		[0-9]*)
+			echo "200 OK with valid uptime field: $uptime"
+			exit 0
+			;;
+		*)
+			echo "Error: Uptime field is missing or invalid"
+			exit 1
+			;;
+		esac
+	else
+		echo "Error: Status code is not 200"
+		exit 1
+	fi
+}
+
+main
