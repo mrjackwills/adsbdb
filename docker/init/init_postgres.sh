2c12417e9a7ac936863cf5a3388b096d2c4f0e6d
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2025-11-04 22:29:58 +0000

    fix: remove /ramdata usage

--- docker/init/init_postgres.sh
+++ docker/init/init_postgres.sh
@@ -34,7 +34,8 @@ set_wal_size() {
 	echo "Setting max_wal_size to 4GB."
 	psql -U "${POSTGRES_USER}" -c "ALTER SYSTEM SET max_wal_size = '4GB';"
 }
-# restore a db from a pg_dump file
+
+# # restore a db from a pg_dump file
 restore_pg_dump() {
 	echo "restore_pg_dump"
 	pg_restore -U "${POSTGRES_USER}" -O --exit-on-error --single-transaction -d "${DB_NAME}" -v /init/pg_dump.tar
@@ -65,38 +66,10 @@ create_tables() {
 
 main() {
 	if [ ! "$1" == "migrations" ]; then
-		echo "running not mig"
 		create_adsbdb_user
 		create_tables
 	fi
-	set_wal_size
 	run_migrations
 }
 
 main "$1"
-
-# Alternative restpre function
-# restore_pg_dump() {
-#   echo "restore_pg_dump"
-
-#   pg_restore -U "$POSTGRES_USER" -O -d "$DB_NAME" -v \
-#              --exclude-index=incoming_request_url_request_url_key \
-#              /init/pg_dump.tar
-
-#   psql -v ON_ERROR_STOP=0 -U "$POSTGRES_USER" -d "$DB_NAME" <<-'EOSQL'
-#       SET maintenance_work_mem='128MB';
-#       CREATE UNIQUE INDEX CONCURRENTLY incoming_request_url_request_url_key
-#              ON incoming_request_url (request_url);
-# EOSQL
-
-#   psql -v ON_ERROR_STOP=0 -U "$POSTGRES_USER" -d "$DB_NAME" <<-'EOSQL'
-#       ALTER TABLE incoming_request_url
-#         ADD CONSTRAINT incoming_request_url_request_url_key
-#         UNIQUE USING INDEX incoming_request_url_request_url_key;
-# EOSQL
-
-#   psql -v ON_ERROR_STOP=0 -U "$POSTGRES_USER" -d "$DB_NAME" <<-'EOSQL'
-#       GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO :DB_NAME;
-#       GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO :DB_NAME;
-# EOSQL
-# }
